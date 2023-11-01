0d774b133cd0437705589b6306687e30f21cbb37
Aditya Wibisana <aditya.wibisana@gmail.com>
2023-11-01 19:27:53 +0700

    Fix already cancelled, but still resuming

--- voicepingintent/src/main/java/com/smartwalkie/voicepingintent/loginusecase/ActionLogin.kt
+++ voicepingintent/src/main/java/com/smartwalkie/voicepingintent/loginusecase/ActionLogin.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -29,7 +30,9 @@ class ActionLogin(context: Context) {
     suspend fun login(context: Context, username: String, password: String) = suspendCoroutine<LoginResult?> {
         val timeout = CoroutineScope(Job()).launch(Dispatchers.IO) {
             delay(10_000)
-            it.resume(LoginFailed("Timeout. Please try again."))
+            if (this.isActive) {
+                it.resume(LoginFailed("Timeout. Please try again."))
+            }
         }
 
         val intentFilter = IntentFilter().apply {
