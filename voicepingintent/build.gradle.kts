9e1b50884c58d2a2d80858fe92b2876733b6143d
Aditya Wibisana <aditya.wibisana@gmail.com>
2023-11-16 15:08:15 +0700

    Add maven task

--- voicepingintent/build.gradle.kts
+++ voicepingintent/build.gradle.kts
@@ -2,7 +2,11 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("maven-publish")
 }
+
+group = "com.github.adityawibisana"
+version = "0.0.1"
 
 android {
     namespace = "com.smartwalkie.voicepingintent"
@@ -31,6 +35,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    publishing {
+        singleVariant("release") {
+            // if you don't want sources/javadoc, remove these lines
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -44,4 +55,29 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("aar") {
+            groupId = "aditya.wibisana"
+            artifactId = "voiceping"
+            version = "0.0.1"
+
+            artifact("$buildDir/outputs/aar/bar-release.aar")
+
+            pom.withXml {
+                val dependencies = asNode().appendNode("dependencies")
+
+                val addNode = { groupId: String, artifactId: String, version: String ->
+                    val dependency = dependencies.appendNode("dependency")
+                    dependency.appendNode("groupId", groupId)
+                    dependency.appendNode("artifactId", artifactId)
+                    dependency.appendNode("version", version)
+                }
+
+                addNode("com.example", "dependency-name", "1.0")
+            }
+        }
+    }
 }
