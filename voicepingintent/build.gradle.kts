2fa8f21e2290e693132e22a1531849c79bb9422d
Aditya Wibisana <aditya.wibisana@gmail.com>
2023-11-17 10:56:18 +0800

    Support Jitpack
    - Use java 17
    - Use gradle 8.1.2
    - Add maven task

--- voicepingintent/build.gradle.kts
+++ voicepingintent/build.gradle.kts
@@ -2,7 +2,11 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
+    id("maven-publish")
 }
+
+group = "com.github.adityawibisana"
+version = "0.0.1"
 
 android {
     namespace = "com.smartwalkie.voicepingintent"
@@ -24,12 +28,19 @@ android {
             )
         }
     }
+    publishing {
+        singleVariant("release") {
+            // if you don't want sources/javadoc, remove these lines
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -44,4 +55,27 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("aar") {
+            groupId = "aditya.wibisana"
+            artifactId = "voiceping"
+            version = "0.0.1"
+
+            pom.withXml {
+                val dependencies = asNode().appendNode("dependencies")
+
+                val addNode = { groupId: String, artifactId: String, version: String ->
+                    val dependency = dependencies.appendNode("dependency")
+                    dependency.appendNode("groupId", groupId)
+                    dependency.appendNode("artifactId", artifactId)
+                    dependency.appendNode("version", version)
+                }
+
+                addNode("com.example", "dependency-name", "1.0")
+            }
+        }
+    }
 }
