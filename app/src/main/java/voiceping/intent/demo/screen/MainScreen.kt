0b42211d97baf5d241327e2a46f096b3c2f3bec9
Aditya Wibisana <aditya.wibisana@gmail.com>
2023-11-30 12:22:30 +0700

    Adjust welcome text

--- app/src/main/java/voiceping/intent/demo/screen/MainScreen.kt
+++ app/src/main/java/voiceping/intent/demo/screen/MainScreen.kt
@@ -1,13 +1,21 @@
 package voiceping.intent.demo.screen
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -35,10 +43,29 @@ fun MainScreen(navController: NavController,
     val context = LocalContext.current.applicationContext
 
     Column(modifier = Modifier.padding(12.dp)) {
-        Text("Use Voiceping within your app! Use single library: https://github.com/adityawibisana/voiceping-intent",
+        val welcomeText = buildAnnotatedString {
+            withStyle(style = SpanStyle(color = Color.Black)) {
+                append("Control Voiceping within your app, with ")
+            }
+            withStyle(style = SpanStyle(color = Color.Blue, textDecoration = TextDecoration.Underline)) {
+                append("this library")
+                addStringAnnotation(
+                    tag = "URL",
+                    annotation = "https://github.com/adityawibisana/voiceping-intent",
+                    start = 0,
+                    end = 11
+                )
+            }
+        }
+        ClickableText(welcomeText,
             style = Typography.bodyLarge,
             modifier = Modifier
-                .padding(12.dp)
+                .padding(12.dp),
+            onClick = { offset ->
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/adityawibisana/voiceping-intent"))
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                context.startActivity(intent)
+            }
         )
 
         Spacer(modifier = Modifier.weight(1.0f))
