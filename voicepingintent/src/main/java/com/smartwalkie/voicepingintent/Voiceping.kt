9adf57527b9572e2b83ba4696b1b40dd73ba1ff8
Aditya Wibisana <aditya.wibisana@gmail.com>
2023-11-30 18:53:24 +0700

    Attempt to rerun Voiceping base service

--- voicepingintent/src/main/java/com/smartwalkie/voicepingintent/Voiceping.kt
+++ voicepingintent/src/main/java/com/smartwalkie/voicepingintent/Voiceping.kt
@@ -1,7 +1,11 @@
 package com.smartwalkie.voicepingintent
 
 import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.core.content.ContextCompat
 
 object Voiceping {
     @SuppressLint("StaticFieldLeak")
@@ -15,6 +19,22 @@ object Voiceping {
             action = VoicepingAction(context.applicationContext)
             state.initialize(context.applicationContext)
             isInitialized = true
+
+            val restartReceiver = object : BroadcastReceiver() {
+                override fun onReceive(c: Context?, intent: Intent?) {
+                    action.openVoiceping(context)
+                }
+            }
+            val intentFilter = IntentFilter().apply {
+                addAction("com.voiceping.store.restarted")
+                addAction("com.voiceping.store.killed")
+            }
+            ContextCompat.registerReceiver(
+                context.applicationContext,
+                restartReceiver,
+                intentFilter,
+                ContextCompat.RECEIVER_EXPORTED
+            )
         }
 
         action.openVoiceping(context)
