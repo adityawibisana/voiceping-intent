bfb4f1d9d829f8255a84c41b29cac7e5fed633c8
Aditya Wibisana <aditya.wibisana@gmail.com>
2024-06-04 12:08:38 +0700

    Increase library version

--- voicepingintent/build.gradle.kts
+++ voicepingintent/build.gradle.kts
@@ -46,13 +46,13 @@ android {
 }
 
 dependencies {
-    val lifecycleVersion = "2.6.2"
+    val lifecycleVersion = "2.8.1"
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation ("com.google.code.gson:gson:2.10.1")
     kapt("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
